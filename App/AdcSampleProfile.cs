bd3047005eb01de284f29434d3a9090c6f07950c
SL <s.larionov@ek.karelia.ru>
2022-09-28 22:11:01 +0300

    ADC max value display

--- App/AdcSampleProfile.cs
+++ App/AdcSampleProfile.cs
@@ -6,43 +6,53 @@ using System.Threading.Tasks;
 
 namespace WinMachine.App
 {
-  public class AdcSampleProfile
-  {
-    public int MinValue { get; private set; }
-
-    public int MaxValue { get; private set; }
-
-    public bool IsDC { get; private set; }
-
-    public bool IsAC => !IsDC;
-
-    public AdcSampleProfile(bool isDC, int minValue, int maxValue)
+    public class AdcSampleProfile
     {
-      IsDC = isDC;
-      MinValue = minValue;
-      MaxValue = maxValue;
+        public int MinInput { get; private set; }
+
+        public int MaxInput { get; private set; }
+
+        public string OutputUnit { get; private set; }
+
+        public int MinOutput { get; private set; }
+
+        public int MaxOutput { get; private set; }
+
+        public bool IsDC { get; private set; }
+
+        public bool IsAC => !IsDC;
+
+        public int InputSpan => (MaxInput - MinInput);
+
+        public int OutputSpan => (MaxOutput - MinOutput);
+
+        public AdcSampleProfile(bool isDC, int minInput, int maxInput, 
+            string outputUnit, int minOutput, int maxOutput)
+        {
+            IsDC = isDC;
+            MinInput = minInput;
+            MaxInput = maxInput;
+            OutputUnit = outputUnit;
+            MinOutput = minOutput;
+            MaxOutput = maxOutput;
+        }
+
+        public double TranslateToRate(int inputValue) 
+            => ((double)(inputValue - MinInput) / InputSpan);
+
+        public double TranslateToOutput(int inputValue) 
+            => MinOutput + (TranslateToRate(inputValue) * OutputSpan);
+
+        public static readonly AdcSampleProfile DC8bit = 
+            new AdcSampleProfile(true, 0, 255, "V", 0, 5);
+
+        // 23.09.2020: 0A=128adc, 1.74A=145adc, 1.74*2.87=5, (145-128=17)*2.87=49
+        // +5A=>128+49=177, -5A=128-49=79
+        public static readonly AdcSampleProfile ACS712_5A8bit = 
+            new AdcSampleProfile(false, 79, 177, "A", -5, 5);
+
+        // (0.5V,4.5V) => (-30A,+30A) from=255*10/100=25 to=255-25=230
+        public static readonly AdcSampleProfile ACS712_30A8bit = 
+            new AdcSampleProfile(false, 25, 230, "A", -30, 30);
     }
-
-    public static readonly AdcSampleProfile DC8bit = new AdcSampleProfile(true, 0, 255);
-
-    // ACS712(5A): 0A=>2.52V=>512adc, -5A=>1.6V=>327adc, 5A=>3.4V=>697adc
-    public static readonly AdcSampleProfile ACS712AC10bit = new AdcSampleProfile(false, 327, 697);
-
-    // Works for both 8bit 5V output and 3V3 8bit output. 
-    // In the case of 5V: 0A=>2.52V=129adc, -5A=1.52V=>129-51=78adc, +5A=>3.52V=>129+51=180adc
-    //public static readonly AdcSampleProfile ACS712AC8bit = new AdcSampleProfile(false, 78, 180);
-
-    // Calibrated with divider: 0A=>133adc, +1.74A=>150adc, 1.74*2.87=5, (150-133=17)*2.87=49
-    // +5A=>133+49=182adc, -5A=>133-49=84
-    //public static readonly AdcSampleProfile ACS712AC8bit = new AdcSampleProfile(false, 84, 182);
-
-    // 23.09.2020: 0A=128adc, 1.74A=145adc, 1.74*2.87=5, (145-128=17)*2.87=49
-    // +5A=>128+49=177, -5A=128-49=79
-    //public static readonly AdcSampleProfile ACS712AC8bit = new AdcSampleProfile(false, 81, 179);
-    // manually adjusted 15/05/2022 to get zero amp at the center (notebook)
-    public static readonly AdcSampleProfile ACS712_5A8bit = new AdcSampleProfile(false, 79, 177);
-
-    // (0.5V,4.5V) => (-30A,+30A) from=255*10/100=25 to=255-25=230
-    public static readonly AdcSampleProfile ACS712_30A8bit = new AdcSampleProfile(false, 25, 230);
-  }
 }
--- App/GraphPainter.cs
+++ App/GraphPainter.cs
@@ -9,98 +9,115 @@ using System.Windows.Media;
 
 namespace WinMachine.App
 {
-  public class GraphPainter
-  {
-    private Size visualSize;
-
-    private const int padWidth = 10;
-
-    private double graphX => padWidth;
-    private double graphY => visualSize.Height - padWidth;
-    private double graphW => visualSize.Width - (padWidth * 2);
-    private double graphH => visualSize.Height - (padWidth * 2);
-
-    public GraphPainter(Size visualSize)
+    public class GraphPainter
     {
-      this.visualSize = visualSize;
-    }
+        private Size visualSize;
 
-    public DrawingVisual DrawAxis()
-    {
-      var drawingVisual = new DrawingVisual();
-      DrawingContext dc = drawingVisual.RenderOpen();
+        private const int padWidth = 10;
 
-      var axisPen = new Pen(new SolidColorBrush(Colors.Silver), 2);
-      var divPen = new Pen(new SolidColorBrush(Colors.Silver), 1);
+        private double graphX => padWidth;
+        private double graphY => visualSize.Height - padWidth;
+        private double graphW => visualSize.Width - (padWidth * 2);
+        private double graphH => visualSize.Height - (padWidth * 2);
 
-      int numDivs = 10;
-
-      // vertical divisor lines
-      double divW = graphW / numDivs;
-      for (int i = 0; i <= numDivs; i++) {
-        double divX = graphX + divW * i;
-        Pen pen = (i == 0 || i == numDivs || i == numDivs / 2) ? axisPen : divPen;
-        dc.DrawLine(pen, new Point(divX, graphY), new Point(divX, graphY - graphH));
-      }
-
-      // horizontal divisor lines
-      double divH = graphH / numDivs;
-      for (int i = 0; i <= numDivs; i++) {
-        double divY = graphY - divH * i;
-        Pen pen = (i == 0 || i == numDivs || i == numDivs / 2) ? axisPen : divPen;
-        dc.DrawLine(pen, new Point(graphX, divY), new Point(graphX + graphW, divY));
-      }
-
-      dc.Close();
-      return drawingVisual;
-    }
-
-    public DrawingVisual DrawGraph(List<int> samples, AdcSampleProfile profile, Color color, int graphId)
-    {
-      var drawingVisual = new DrawingVisual();
-      DrawingContext dc = drawingVisual.RenderOpen();
-
-      var graphBrush = new SolidColorBrush(color);
-      var graphPen = new Pen(graphBrush, 2);
-
-      string text = graphId.ToString();
-      var typeface = new Typeface("Arial");
-      double emSize = (graphH / 20);
-      double pixelsPerDip = 1;
-      var ft = new FormattedText(text, CultureInfo.CurrentCulture,
-        FlowDirection.LeftToRight, typeface, emSize, graphBrush, pixelsPerDip);
-      dc.DrawText(ft, new Point(graphX + (ft.Width * graphId), graphY - ft.Height));
-
-      int numPoints = samples.Count();
-      if (numPoints > 1) {
-
-        int profileH = (profile.MaxValue - profile.MinValue);
-        double pointX = graphX;
-
-        // number of line segments is number of points - 1
-        double pointStep = graphW / (numPoints - 1);
-
-        var lastPoint = new Point();
-        foreach (var sample in samples) {
-
-          double pointH = ((double)(sample - profile.MinValue) / profileH) * graphH;
-          double pointY = graphY - pointH;
-          var point = new Point(pointX, pointY);
-
-          double pointRadius = graphPen.Thickness / 2;
-          dc.DrawEllipse(graphBrush, graphPen, point, pointRadius, pointRadius);
-
-          if (pointX > graphX) {
-            dc.DrawLine(graphPen, lastPoint, point);
-          }
-
-          lastPoint = point;
-          pointX += pointStep;
+        public GraphPainter(Size visualSize)
+        {
+            this.visualSize = visualSize;
         }
-      }
 
-      dc.Close();
-      return drawingVisual;
+        public DrawingVisual DrawAxis()
+        {
+            var drawingVisual = new DrawingVisual();
+            DrawingContext dc = drawingVisual.RenderOpen();
+
+            var axisPen = new Pen(new SolidColorBrush(Colors.Silver), 2);
+            var divPen = new Pen(new SolidColorBrush(Colors.Silver), 1);
+
+            int numDivs = 10;
+
+            // vertical divisor lines
+            double divW = graphW / numDivs;
+            for (int i = 0; i <= numDivs; i++)
+            {
+                double divX = graphX + divW * i;
+                Pen pen = (i == 0 || i == numDivs || i == numDivs / 2) ? axisPen : divPen;
+                dc.DrawLine(pen, new Point(divX, graphY), new Point(divX, graphY - graphH));
+            }
+
+            // horizontal divisor lines
+            double divH = graphH / numDivs;
+            for (int i = 0; i <= numDivs; i++)
+            {
+                double divY = graphY - divH * i;
+                Pen pen = (i == 0 || i == numDivs || i == numDivs / 2) ? axisPen : divPen;
+                dc.DrawLine(pen, new Point(graphX, divY), new Point(graphX + graphW, divY));
+            }
+
+            dc.Close();
+            return drawingVisual;
+        }
+
+        private void DrawGraphLegend(
+            DrawingContext dc, List<int> samples, AdcSampleProfile profile, Color color, int graphId)
+        {
+            var graphBrush = new SolidColorBrush(color);
+            int maxInput = samples.Max();
+            double maxOutput = profile.TranslateToOutput(maxInput);
+
+            string text = 
+                $"{graphId}: ({profile.MinOutput},{profile.MaxOutput})" +
+                $" MAX={maxOutput:f2}{profile.OutputUnit}";
+
+            var typeface = new Typeface("Arial");
+            double emSize = (graphH / 20);
+            double pixelsPerDip = 1;
+
+            var ft = new FormattedText(text, CultureInfo.CurrentCulture,
+              FlowDirection.LeftToRight, typeface, emSize, graphBrush, pixelsPerDip);
+            dc.DrawText(ft, new Point(graphX, graphY - (ft.Height * graphId) - ft.Height));
+        }
+
+        public DrawingVisual DrawGraph(
+            List<int> samples, AdcSampleProfile profile, Color color, int graphId)
+        {
+            var drawingVisual = new DrawingVisual();
+            DrawingContext dc = drawingVisual.RenderOpen();
+
+            var graphBrush = new SolidColorBrush(color);
+            var graphPen = new Pen(graphBrush, 2);
+
+            DrawGraphLegend(dc, samples, profile, color, graphId);
+
+            int numPoints = samples.Count();
+            if (numPoints > 1)
+            {
+                double pointX = graphX;
+
+                // number of line segments is number of points - 1
+                double pointStep = graphW / (numPoints - 1);
+
+                var lastPoint = new Point();
+                foreach (var sample in samples)
+                {
+                    double pointH = profile.TranslateToRate(sample) * graphH;
+                    double pointY = graphY - pointH;
+                    var point = new Point(pointX, pointY);
+
+                    double pointRadius = graphPen.Thickness / 2;
+                    dc.DrawEllipse(graphBrush, graphPen, point, pointRadius, pointRadius);
+
+                    if (pointX > graphX)
+                    {
+                        dc.DrawLine(graphPen, lastPoint, point);
+                    }
+
+                    lastPoint = point;
+                    pointX += pointStep;
+                }
+            }
+
+            dc.Close();
+            return drawingVisual;
+        }
     }
-  }
 }
